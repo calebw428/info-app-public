ec25027a3ddf8424bf51a24ffbbe2f8ca453db03
User One <uone@localhost.localdomain>
2018-03-10 16:59:18 -0700

    005: add date, README third edit

--- sources/InfoMain.java
+++ sources/InfoMain.java
@@ -1,6 +1,16 @@
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
 public class InfoMain {
 
 	public static void main (String[] args) {
 		System.out.println("User name:\t" + System.getProperty("user.name"));
+		LocalDate ld = LocalDate.now();
+		System.out.print("Date:\t\t");
+		System.out.println(String.format("%s %d, %d", 
+		  ld.getMonth().getDisplayName(TextStyle.FULL, Locale.US), 
+		  ld.getDayOfMonth(), 
+		  ld.getYear()));
 	}
 }
